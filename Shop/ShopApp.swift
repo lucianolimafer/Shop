e9ed64c9efa3cbd11acdb8eeccd6ee6e130db52c
Luciano Lima Fer <llferreira@rd.com.br>
2024-05-18 00:02:51 -0300

    Initial Commit

---
+++ Shop/ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopApp.swift
+//  Shop
+//
+//  Created by Luciano Lima Ferreira on 18/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
