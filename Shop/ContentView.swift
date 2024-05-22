4e30b02744e576238b275507af70e42163434674
Luciano Lima Fer <llferreira@rd.com.br>
2024-05-22 02:47:03 -0300

    ⚙️ add an bottom tab

--- Shop/ContentView.swift
+++ Shop/ContentView.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.colorScheme) var colorScheme
+    
     var body: some View {
-        Text("ContentView")
+        TabView {
+            Home()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Início")
+                }
+                .toolbarBackground(.visible, for: .tabBar)
+                .toolbarBackground(.ultraThickMaterial, for: .tabBar)
+            
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Procurar")
+                }
+                .toolbarBackground(.visible, for: .tabBar)
+                .toolbarBackground(.ultraThickMaterial, for: .tabBar)
+            
+            Text("Cart")
+                .tabItem {
+                    Image(systemName: "bag")
+                    Text("Carrinho")
+                }
+                .toolbarBackground(.visible, for: .tabBar)
+                .toolbarBackground(.ultraThickMaterial, for: .tabBar)
+            
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("Perfil")
+                }
+                .toolbarBackground(.visible, for: .tabBar)
+                .toolbarBackground(.ultraThickMaterial, for: .tabBar)
+        }
+        .tint(colorScheme == .dark ? .white : .black)
     }
 }
 
